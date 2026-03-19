7d086174dca37ce7837dde208af0fcfca244ebaf
Wiktor <wiko700@gmail.com>
2026-03-19 22:14:23 +0100

    refactor: prepare demo scenario

--- Program.cs
+++ Program.cs
@@ -2,6 +2,13 @@
 using APBD_Cw1_s30359.models.rentals;
 using APBD_Cw1_s30359.Models.Users;
 
+// 11. Devices
+var camera1 = new Camera("Canon", "EOS 250D", 2021, "CAM001", 24, true);
+var laptop1 = new Laptop("Dell", "XPS 13", 2022, "LAP001", 16, true);
+var projector1 = new Projector("Epson", "EB-X41", 2021, "PROJ001", 3600, true);
+var projector2 = new Projector("BenQ", "MH550", 2020, "PROJ002", 3500, false);
+
+// 12. Users
 var employee1 = new Employee("Jan", "Kowalski", new DateOnly(1980, 5, 12));
 var employee2 = new Employee("Anna", "Nowak", new DateOnly(1975, 11, 3));
 
@@ -9,38 +16,34 @@ var student1 = new Student("Piotr", "Wiśniewski", new DateOnly(2002, 3, 15));
 var student2 = new Student("Katarzyna", "Wójcik", new DateOnly(2001, 7, 22));
 var student3 = new Student("Michał", "Kamiński", new DateOnly(2003, 1, 10));
 
-var camera1 = new Camera("Canon", "EOS 250D", 2021, "CAM001", 24, true);
-var camera2 = new Camera("Nikon", "D3500", 2020, "CAM002", 24, true);
-var camera3 = new Camera("Sony", "Alpha a6400", 2022, "CAM003", 24, false);
+// 13. Valid rental
+var rental1 = new Rental(student1, camera1, new DateTime(2026, 03, 17, 9, 30, 0), DateTime.Now.AddDays(1));
 
-var laptop1 = new Laptop("Dell", "XPS 13", 2022, "LAP001", 16, true);
-var laptop2 = new Laptop("Lenovo", "ThinkPad E14", 2021, "LAP002", 8, false);
-var laptop3 = new Laptop("Apple", "MacBook Air M1", 2020, "LAP003", 8, false);
+// 14. Invalid rental (camera1 is already rented by student1)
+var rental2 = new Rental(student2, camera1, new DateTime(2026, 03, 18, 9, 30, 0), new DateTime(2026, 03, 20, 9, 30, 0));
 
-var projector1 = new Projector("Epson", "EB-X41", 2021, "PROJ001", 3600, true);
-var projector2 = new Projector("BenQ", "MH550", 2020, "PROJ002", 3500, false);
-var projector3 = new Projector("ViewSonic", "PA503S", 2022, "PROJ003", 3800, true);
+// 14. Invalid rental (trying to exceed student limit)
 
-Device.DisplayAllDevicesWithStatus();
-Device.DisplayAvailableDevices();
+var rental3 = new Rental(student1, projector1, new DateTime(2026, 03, 18, 9, 30, 0), new DateTime(2026, 03, 20, 9, 30, 0));
+var rental4 = new Rental(student1, projector2,  new DateTime(2026, 03, 18, 9, 30, 0), new DateTime(2026, 03, 20, 9, 30, 0));
 
-var rental1 = new Rental(student1, camera1, new DateTime(2026, 03, 17, 9, 30, 0), new DateTime(2026, 03, 17, 21, 30, 0));
-var rental2 = new Rental(student1, laptop1, new DateTime(2026, 03, 18, 9, 30, 0), new DateTime(2026, 03, 20, 9, 30, 0));
-
-var rental3 = new Rental(employee1, camera2, new DateTime(2026, 03, 18, 9, 30, 0), new DateTime(2026, 03, 20, 9, 30, 0));
-var rental4 = new Rental(employee1, camera3,  new DateTime(2026, 03, 18, 9, 30, 0), new DateTime(2026, 03, 20, 9, 30, 0));
-var rental5 = new Rental(employee1, laptop2, new DateTime(2026, 03, 18, 9, 30, 0), new DateTime(2026, 03, 20, 9, 30, 0));
-var rental6 = new Rental(employee1, laptop3, new DateTime(2026, 03, 18, 9, 30, 0), new DateTime(2026, 03, 20, 9, 30, 0));
-var rental7 = new Rental(employee1, projector1, new DateTime(2026, 03, 18, 9, 30, 0), new DateTime(2026, 03, 20, 9, 30, 0));
-
-
-Rental.DisplayRentalsForPerson(student1);
-
+// 15. End of rental before deadline
 rental1.End();
 
+// 16. End of rental 2 days after deadline (fine for 2 days)
+var rental5 = new Rental(employee1, laptop1, DateTime.Now.AddDays(-5), DateTime.Now.AddDays(-2).AddHours(1));
+rental5.End();
+
+// Displaying all devices with status
+Device.DisplayAllDevicesWithStatus();
+
+// Displaying available devices
+Device.DisplayAvailableDevices();
+
+// Displaying rentals for student1
 Rental.DisplayRentalsForPerson(student1);
 
 
-var rental8 = new Rental(student1, camera1, new DateTime(2026, 03, 17, 9, 30, 0), new DateTime(2026, 03, 17, 21, 30, 0));
-
+// Displaying rentals after deadline
 Rental.DisplayRentalsAfterDeadline();
+
--- Services/RentalFineService.cs
+++ Services/RentalFineService.cs
@@ -4,6 +4,8 @@ namespace APBD_Cw1_s30359.Services;
 
 public class RentalFineService(Rental rental)
 {
+    private const decimal FinePerDay = 5m;
+
     public void CheckIfFineNecessary(DateTime realRentalEnd)
     {
         var difference = realRentalEnd - rental.RentalEnd; 
@@ -11,8 +13,7 @@ public class RentalFineService(Rental rental)
         if (realRentalEnd > rental.RentalEnd)
         {
             var lateDays = (int) Math.Ceiling(difference.TotalDays); 
-            var finePerDay = 5m; 
-            var totalFine = lateDays * finePerDay;
+            var totalFine = lateDays * FinePerDay;
 
             Console.WriteLine(
                 $"[{rental.Renter.FirstName} {rental.Renter.LastName}] " +
